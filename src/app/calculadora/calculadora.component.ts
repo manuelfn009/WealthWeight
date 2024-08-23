95d57097554f55e9c78f56a8f9ca94f11d7c2933
Manuel Fernández <manuelfn066@gmail.com>
2024-08-23 11:43:34 +0200

    Update calculadora.component.ts

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -15,41 +15,45 @@ export class CalculadoraComponent {
   error = '';
   color = 'white';
   bg_color = 'white';
-  
+
   calcularIMC() {
     //prevent default
     addEventListener('click', (event) => event.preventDefault());
-
+    this.imc = 0
     if (this.altura.value && this.peso.value) {
-      this.imc = +this.peso.value / (+this.altura.value * +this.altura.value);
-      this.imc = +this.imc.toFixed(2);
-      console.log(this.imc.toFixed(2));
-      if (this.imc < 18.5) {
-        this.error = 'Bajo Peso';
-        this.color = '#0390fc';
-        this.bg_color = '#88cafc';
-      } else if (this.imc >= 18.5 && this.imc <= 24.9) {
-        this.error = 'Normal';
-        this.color = '#02b82f';
-        this.bg_color = '#a8fca8';
-      } else if (this.imc >= 25 && this.imc <= 29.9) {
-        this.error = 'Sobrepeso';
-        this.color = '#fcba03';
-        this.bg_color = '#fcd8a8';
-      } else if (this.imc >= 30 && this.imc <= 34.9) {
-        this.error = 'Obesidad I';
-        this.color = '#b80202';
-        this.bg_color = '#fca8a8';
-      } else if (this.imc >= 35 && this.imc <= 39.9) {
-        this.error = 'Obesidad II (Severa)';
-        this.color = '#b80202';
-        this.bg_color = '#fca8a8';
-      } else if (this.imc >= 40) {
-        this.error = 'Obesidad III (Mórbida)';
-        this.color = '#b80202';
-        this.bg_color = '#fca8a8';
+      if (+this.peso.value < 0 || +this.altura.value < 0) {
+        this.error = "Debe ser valores positivos"
+      } else {
+        this.imc = +this.peso.value / (+this.altura.value * +this.altura.value);
+        this.imc = +this.imc.toFixed(2);
+        console.log(this.imc.toFixed(2));
+        if (this.imc < 18.5) {
+          this.error = 'Bajo Peso';
+          this.color = '#0390fc';
+          this.bg_color = '#88cafc';
+        } else if (this.imc >= 18.5 && this.imc <= 24.9) {
+          this.error = 'Normal';
+          this.color = '#02b82f';
+          this.bg_color = '#a8fca8';
+        } else if (this.imc >= 25 && this.imc <= 29.9) {
+          this.error = 'Sobrepeso';
+          this.color = '#fcba03';
+          this.bg_color = '#fcd8a8';
+        } else if (this.imc >= 30 && this.imc <= 34.9) {
+          this.error = 'Obesidad I';
+          this.color = '#b80202';
+          this.bg_color = '#fca8a8';
+        } else if (this.imc >= 35 && this.imc <= 39.9) {
+          this.error = 'Obesidad II (Severa)';
+          this.color = '#b80202';
+          this.bg_color = '#fca8a8';
+        } else if (this.imc >= 40) {
+          this.error = 'Obesidad III (Mórbida)';
+          this.color = '#b80202';
+          this.bg_color = '#fca8a8';
+        }
       }
-    }else{
+    } else {
       this.error = 'Debes de rellenar todos los campos';
       console.log('Error');
     }
