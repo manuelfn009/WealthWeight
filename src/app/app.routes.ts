73967595e8a14a0b00bee9973a479a10941ed998
Manuel Fernández <manuelfn066@gmail.com>
2024-09-11 11:00:40 +0200

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
-    {path: "calculadora", component: CalculadoraComponent}
+    {path: "calculadora", component: CalculadoraComponent},
+    {path: "", component: InicioComponent},
+    {path: "inicio", component: InicioComponent}
 ];
